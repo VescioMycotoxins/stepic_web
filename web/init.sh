209cd20f1818865ca0f0e028710e33127316a1d3
VescioMycotoxins <VescioMycotoxins@gmail.com>
2021-11-05 10:30:27 +0300

    gunicorn add

--- web/init.sh
+++ web/init.sh
@@ -1,10 +1,7 @@
-
-
-#sudo /etc/init.d/gunicorn web.hello:myapp
-#sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn reload
-
 sudo /etc/init.d/nginx start
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo gunicorn web.hello:myapp
+
 #sudo /etc/init.d/mysql start
