d79e8bce01cc5ab54242e5cd0e31830e0d666f3d
VescioMycotoxins <VescioMycotoxins@gmail.com>
2021-11-05 05:38:58 +0300

    gunicorn add

--- web/hello.py
+++ web/hello.py
@@ -1,8 +1,6 @@
-
 def myapp(environ, start_response):
     status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
+    response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    body = "\n".join(environ["QUERY_STRING"][2:].split("&"))
+    body = b"\n".join(environ["QUERY_STRING"][2:].split("&"))
     return [body]
-
