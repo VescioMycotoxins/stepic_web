25bd8078063b7100b21d3b38666b66c338582f05
VescioMycotoxins <VescioMycotoxins@gmail.com>
2021-11-05 10:49:59 +0300

    gunicorn add

--- web/hello.py
+++ web/hello.py
@@ -1,9 +1,9 @@
 def app(environ, start_response):
-    data = [bytes((i+ '\n'), 'utf-8') for i in environ["QUERY_STRING"].split("&")]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return data
+    return body
