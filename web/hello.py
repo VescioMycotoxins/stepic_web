3a9a685a16b02a4069afc5a6a5dc0ab8e284403b
VescioMycotoxins <VescioMycotoxins@gmail.com>
2021-11-05 08:38:51 +0300

    gunicorn add

--- web/hello.py
+++ web/hello.py
@@ -2,5 +2,5 @@ def myapp(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    body = b"\n".join(environ["QUERY_STRING"][2:].split("&"))
+    body = "\n".join(environ["QUERY_STRING"][2:].split("&"))
     return [body]
--- web/init.sh
+++ web/init.sh
@@ -1,8 +1,8 @@
 
 
-sudo gunicorn -b 0.0.0.0:8080 web:hello
+sudo /etc/init.d/gunicorn web.hello:myapp
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn reload
 
 sudo /etc/init.d/nginx start
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
